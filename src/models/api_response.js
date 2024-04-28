a725656fd8d85083dfa598809993b0785805c071
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2024-04-28 23:51:45 +0530

    feat: api response factory function

---
+++ src/models/api_response.js
@@ -0,0 +1,29 @@
+/**
+ * @param {{ success: boolean, status: { code: "string", message: "string" } | undefined, data: object | null }} Options
+ * @returns {{ success: boolean, status: { code: "string", message: "string" }, data: object | null }}
+ */
+function createApiResponse({ success, status = undefined, data = null }) {
+	if (status === undefined) {
+		if (success) {
+			status = {
+				code: "ok",
+				message: "OK",
+			};
+		} else {
+			status = {
+				code: "unknownError",
+				message: "Unknown error occurred",
+			};
+		}
+	}
+
+	const apiResponse = {
+		success,
+		status,
+		data,
+	};
+
+	return apiResponse;
+}
+
+module.exports = { createApiResponse };
