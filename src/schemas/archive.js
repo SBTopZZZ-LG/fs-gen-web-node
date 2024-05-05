0da866aa8fbc064246d8e2312031e56b75ca0463
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2024-05-05 19:42:52 +0530

    feat: archive mongoose schema

---
+++ src/schemas/archive.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const DOCUMENT_VALIDITY_IN_SECONDS = 600;
+
+const schema = new mongoose.Schema(
+	{
+		data: {
+			type: mongoose.Types.Buffer,
+			required: true,
+		},
+		expiresAfter: {
+			type: Date,
+			default: () => Date.now() + DOCUMENT_VALIDITY_IN_SECONDS * 1000,
+			expires: DOCUMENT_VALIDITY_IN_SECONDS,
+		},
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			transform: (_, ret) => {
+				delete ret.__v;
+			},
+		},
+	}
+);
+
+const modelledSchema = mongoose.model("archive", schema);
+module.exports = modelledSchema;
